7e04ada7668613122b28d9be5163b5d8a092ad93
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-02-19 11:45:24 +0100

    Move archi to cpython&pybind11

--- pythonscript/config.py
+++ pythonscript/config.py
@@ -2,19 +2,23 @@ from os import path
 import subprocess
 
 
-MP_DIR = path.dirname(path.abspath(__file__)) + '/micropython'
-MP_TARGET = MP_DIR + "/libmicropython.a"
+PY_DIR = path.dirname(path.abspath(__file__)) + '/cpython'
+PY_TARGET = PY_DIR + "/libpython3.6m.a"
+PY_DEBUG_TARGET = PY_DIR + "/libpython3.6dm.a"
 
 
 def can_build(platform):
-  return True
+    return True
 
 
 def configure(env):
-    if not path.isfile(MP_TARGET):
-        print('Building libmicropython.a...')
-        cmd = ['make']
-        if env["target"] == "debug":
-            cmd.append('DEBUG=y')
-        subprocess.call(cmd, cwd=MP_DIR)
-        print('libmicropython.a successfully built !')
+    if ((env["target"] == "debug" and not path.isfile(PY_DEBUG_TARGET)) or
+            not path.isfile(PY_TARGET)):
+        print('Building libpython...')
+        if not path.isfile('Makefile'):
+            cmd = ['./configure', '--enable-shared --prefix=%s/build' % PY_DIR]
+            if env["target"] == "debug":
+                cmd.append('--with-pydebug')
+            subprocess.call(cmd, cwd=PY_DIR)
+        subprocess.call(['make'], cwd=PY_DIR)
+        print('libpython successfully built !')
--- pythonscript/register_types.cpp
+++ pythonscript/register_types.cpp
@@ -1,35 +1,28 @@
 // Pythonscript imports
-#include "py_language.h"
-#include "py_script.h"
-#include "py_loader.h"
+// #include "Python.h"
+#include <pybind11/pybind11.h>
+#include <pybind11/eval.h>
 
+namespace py = pybind11;
 
-PyLanguage *script_language_py = NULL;
-ResourceFormatLoaderPyScript *resource_loader_py = NULL;
-ResourceFormatSaverPyScript *resource_saver_py = NULL;
 
 
 void register_pythonscript_types() {
-
-    ClassDB::register_class<PyScript>();
-
-    script_language_py = memnew(PyLanguage);
-    ScriptServer::register_language(script_language_py);
-
-    resource_loader_py = memnew(ResourceFormatLoaderPyScript);
-    ResourceLoader::add_resource_format_loader(resource_loader_py);
-    resource_saver_py = memnew(ResourceFormatSaverPyScript);
-    ResourceSaver::add_resource_format_saver(resource_saver_py);
+    Py_SetProgramName(L"godot");  /* optional but recommended */
+    Py_SetPythonHome(PYTHON_HOME);
+    Py_Initialize();
+    py::eval<py::eval_statements>("import sys\n"
+                                  "print(sys.path)\n");
+    // PyRun_SimpleString("import sys\n"
+    //                    "print(sys.path)\n");
+    // PyRun_SimpleString("from time import time,ctime\n"
+    //                    "print('Today is', ctime(time()))\n");
+    if (Py_FinalizeEx() < 0) {
+        exit(120);
+    }
+    // PyMem_RawFree(program);
 }
 
 
 void unregister_pythonscript_types() {
-    ScriptServer::unregister_language(script_language_py);
-
-    if (script_language_py)
-        memdelete(script_language_py);
-    if (resource_loader_py)
-        memdelete(resource_loader_py);
-    if (resource_saver_py)
-        memdelete(resource_saver_py);
 }
