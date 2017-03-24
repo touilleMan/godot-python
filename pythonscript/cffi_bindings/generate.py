887c6ba27907ffb3af51f7494628c92af1c5cded
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-03-24 20:59:00 +0100

    Bind classdb methods with cffi

--- pythonscript/cffi_bindings/generate.py
+++ pythonscript/cffi_bindings/generate.py
@@ -47,8 +47,14 @@ def py_instance_set_godot_obj(instance_handle, godot_obj):
 # with open(BASEDIR + "../../modules/dlscript/godot.h", 'r') as fd:
 #     ffibuilder.cdef()
 ffibuilder.cdef("""
+// Atomic types
+typedef bool godot_bool;
+typedef int godot_int;
+typedef float godot_real;
+typedef double godot_real64; // for Variant in 3.0
 typedef void godot_object;
 
+// Method binds
 typedef struct godot_method_bind {
     uint8_t _dont_touch_that[1]; // TODO
 } godot_method_bind;
@@ -56,7 +62,9 @@ typedef struct godot_method_bind {
 godot_method_bind *godot_method_bind_get_method(const char *p_classname, const char *p_methodname);
 void godot_method_bind_ptrcall(godot_method_bind *p_method_bind, godot_object *p_instance, const void **p_args, void *p_ret);
 
+godot_object *godot_global_get_singleton(char *p_name); // result shouldn't be freed
 
+// TODO remove this custom stuff
 char **godot_get_class_list();
 typedef godot_object *(*godot_class_constructor)();
 godot_class_constructor godot_get_class_constructor(const char *p_classname);
@@ -69,25 +77,116 @@ const char *godot_get_class_parent(const char *p_classname);
 typedef float godot_real;
 typedef double godot_real64;
 
+// vector2
 typedef struct godot_vector2 {
     uint8_t _dont_touch_that[8];
 } godot_vector2;
-
 void godot_vector2_new(godot_vector2 *p_v, const godot_real p_x, const godot_real p_y);
-
 void godot_vector2_set_x(godot_vector2 *p_v, const godot_real p_x);
 void godot_vector2_set_y(godot_vector2 *p_v, const godot_real p_y);
 godot_real godot_vector2_get_x(const godot_vector2 *p_v);
 godot_real godot_vector2_get_y(const godot_vector2 *p_v);
-
 void godot_vector2_normalize(godot_vector2 *p_v);
 void godot_vector2_normalized(godot_vector2 *p_dest, const godot_vector2 *p_src);
-
 godot_real godot_vector2_length(const godot_vector2 *p_v);
 godot_real godot_vector2_length_squared(const godot_vector2 *p_v);
-
 godot_real godot_vector2_distance_to(const godot_vector2 *p_a, const godot_vector2 *p_b);
 godot_real godot_vector2_distance_squared_to(const godot_vector2 *p_a, const godot_vector2 *p_b);
+
+// godot_string.h
+typedef struct godot_string {
+    uint8_t _dont_touch_that[8];
+} godot_string;
+void godot_string_new(godot_string *p_str);
+void godot_string_new_data(godot_string *p_str, const char *p_contents, const int p_size);
+void godot_string_get_data(const godot_string *p_str, wchar_t *p_dest, int *p_size);
+void godot_string_copy_string(const godot_string *p_dest, const godot_string *p_src);
+wchar_t *godot_string_operator_index(godot_string *p_str, const godot_int p_idx);
+const wchar_t *godot_string_c_str(const godot_string *p_str);
+godot_bool godot_string_operator_equal(const godot_string *p_a, const godot_string *p_b);
+godot_bool godot_string_operator_less(const godot_string *p_a, const godot_string *p_b);
+void godot_string_operator_plus(godot_string *p_dest, const godot_string *p_a, const godot_string *p_b);
+
+
+// godot_variant.h
+typedef struct godot_variant {
+    uint8_t _dont_touch_that[24];
+} godot_variant;
+
+
+// godot_array.h
+typedef struct godot_array {
+    uint8_t _dont_touch_that[8];
+} godot_array;
+void godot_array_set(godot_array *p_arr, const godot_int p_idx, const godot_variant *p_value);
+godot_variant *godot_array_get(godot_array *p_arr, const godot_int p_idx);
+void godot_array_append(godot_array *p_arr, const godot_variant *p_value);
+void godot_array_clear(godot_array *p_arr);
+godot_int godot_array_count(godot_array *p_arr, const godot_variant *p_value);
+godot_bool godot_array_empty(const godot_array *p_arr);
+void godot_array_erase(godot_array *p_arr, const godot_variant *p_value);
+godot_variant godot_array_front(const godot_array *p_arr);
+godot_variant godot_array_back(const godot_array *p_arr);
+godot_int godot_array_find(const godot_array *p_arr, const godot_variant *p_what, const godot_int p_from);
+godot_int godot_array_find_last(const godot_array *p_arr, const godot_variant *p_what);
+godot_bool godot_array_has(const godot_array *p_arr,const  godot_variant *p_value);
+uint32_t godot_array_hash(const godot_array *p_arr);
+void godot_array_insert(godot_array *p_arr, const godot_int p_pos, const godot_variant *p_value);
+void godot_array_invert(godot_array *p_arr);
+godot_bool godot_array_is_shared(const godot_array *p_arr);
+godot_variant godot_array_pop_back(godot_array *p_arr);
+godot_variant godot_array_pop_front(godot_array *p_arr);
+void godot_array_push_back(godot_array *p_arr, const godot_variant *p_value);
+void godot_array_push_front(godot_array *p_arr, const godot_variant *p_value);
+void godot_array_remove(godot_array *p_arr, const godot_int p_idx);
+void godot_array_resize(godot_array *p_arr, const godot_int p_size);
+godot_int godot_array_rfind(const godot_array *p_arr, const godot_variant *p_what, const godot_int p_from);
+godot_int godot_array_size(const godot_array *p_arr);
+void godot_array_sort(godot_array *p_arr);
+void godot_array_sort_custom(godot_array *p_arr, godot_object *p_obj, const godot_string *p_func);
+void godot_array_destroy(godot_array *p_arr);
+
+
+// godot_pool_arrays.h -- string array
+typedef struct godot_pool_string_array {
+    uint8_t _dont_touch_that[8];
+} godot_pool_string_array;
+void godot_pool_string_array_new(godot_pool_string_array *p_psa);
+void godot_pool_string_array_new_with_array(godot_pool_string_array *p_psa, const godot_array *p_a);
+void godot_pool_string_array_append(godot_pool_string_array *p_psa, const godot_string *p_data);
+void godot_pool_string_array_append_array(godot_pool_string_array *p_psa, const godot_pool_string_array *p_array);
+int godot_pool_string_array_insert(godot_pool_string_array *p_psa, const godot_int p_idx, const godot_string *p_data);
+void godot_pool_string_array_invert(godot_pool_string_array *p_psa);
+void godot_pool_string_array_push_back(godot_pool_string_array *p_psa, const godot_string *p_data);
+void godot_pool_string_array_remove(godot_pool_string_array *p_psa, const godot_int p_idx);
+void godot_pool_string_array_resize(godot_pool_string_array *p_psa, const godot_int p_size);
+void godot_pool_string_array_set(godot_pool_string_array *p_psa, const godot_int p_idx, const godot_string *p_data);
+godot_string godot_pool_string_array_get(godot_pool_string_array *p_psa, const godot_int p_idx);
+godot_int godot_pool_string_array_size(godot_pool_string_array *p_psa);
+void godot_pool_string_array_destroy(godot_pool_string_array *p_psa);
+
+// godot_dictionary.h
+typedef struct godot_dictionary {
+    uint8_t _dont_touch_that[8];
+} godot_dictionary;
+void godot_dictionary_new(godot_dictionary *p_dict);
+void godot_dictionary_clear(godot_dictionary *p_dict);
+godot_bool godot_dictionary_empty(const godot_dictionary *p_dict);
+void godot_dictionary_erase(godot_dictionary *p_dict, const godot_variant *p_key);
+godot_bool godot_dictionary_has(const godot_dictionary *p_dict, const godot_variant *p_key);
+godot_bool godot_dictionary_has_all(const godot_dictionary *p_dict, const godot_array *p_keys);
+uint32_t godot_dictionary_hash(const godot_dictionary *p_dict);
+godot_array godot_dictionary_keys(const godot_dictionary *p_dict);
+godot_int godot_dictionary_parse_json(godot_dictionary *p_dict, const godot_string *p_json);
+godot_variant *godot_dictionary_operator_index(godot_dictionary *p_dict, const godot_variant *p_key);
+godot_int godot_dictionary_size(const godot_dictionary *p_dict);
+godot_string godot_dictionary_to_json(const godot_dictionary *p_dict);
+godot_array godot_dictionary_values(const godot_dictionary *p_dict);
+void godot_dictionary_destroy(godot_dictionary *p_dict);
+
+
+godot_string godot_variant_as_string(const godot_variant *p_v);
+godot_dictionary godot_variant_as_dictionary(const godot_variant *p_v);
 """)
 
 # ffibuilder.compile(target="pythonscriptcffi.*", verbose=True)
--- pythonscript/cffi_bindings/mod_godot_bindings.py
+++ pythonscript/cffi_bindings/mod_godot_bindings.py
@@ -51,9 +51,8 @@ def build_class(classname):
         '_gd_constructor': lib.godot_get_class_constructor(cclassname)
     }
     # Methods
-    for crawmeth in iter_on_c(lib.godot_get_class_methods(cclassname)):
-        cmethname = ffi.string(crawmeth)
-        methname = cmethname.decode()
+    for meth in ClassDB.get_class_methods(classname):
+        methname = meth['name']
         # methbind = lib.godot_method_bind_get_method(classname, methname)
         # def bind(self, *args):
         #     lib.godot_method_bind_get_method(methbind)
@@ -79,45 +78,86 @@ def build_class(classname):
     return type(classname, bases, nmspc)
 
 
-def get_class_list():
-    instance = lib.godot_global_get_singleton(b"ClassDB")
-    meth = lib.godot_method_bind_get_method(b"_ClassDB", b"get_class_list")
-    ret = ffi.new("godot_pool_string_array*")
-    lib.godot_pool_string_array_new(ret)
-    lib.godot_method_bind_ptrcall(meth, instance, ffi.NULL, ret)
+class ClassDB:
+    _instance = lib.godot_global_get_singleton(b"ClassDB")
+    _meth_get_class_list = lib.godot_method_bind_get_method(b"_ClassDB", b"get_class_list")
+    _meth_get_method_list = lib.godot_method_bind_get_method(b"_ClassDB", b"get_method_list")
+    _meth_get_parent_class = lib.godot_method_bind_get_method(b"_ClassDB", b"get_parent_class")
 
-    # Convert Godot return into Python civilized stuff
-    unordered = []
-    for i in range(lib.godot_pool_string_array_size(ret)):
-        godot_str = lib.godot_pool_string_array_get(ret, i)
-        c_str = lib.godot_string_c_str(ffi.new('godot_string*', godot_str))
-        unordered.append(ffi.string(c_str))
+    @classmethod
+    def get_class_list(cls):
+        ret = ffi.new("godot_pool_string_array*")
+        lib.godot_pool_string_array_new(ret)
+        lib.godot_method_bind_ptrcall(cls._meth_get_class_list, cls._instance, ffi.NULL, ret)
 
-    # Order class to have a parent defined before their children
-    classes = []
-    while len(unordered) != len(classes):
-        for classname in unordered:
-            parentname = get_class_parent(classname)
-            if not parentname or parentname in classes:
-                if classname not in classes:
-                    classes.append(classname)
+        # Convert Godot return into Python civilized stuff
+        unordered = []
+        for i in range(lib.godot_pool_string_array_size(ret)):
+            godot_str = lib.godot_pool_string_array_get(ret, i)
+            c_str = lib.godot_string_c_str(ffi.new('godot_string*', godot_str))
+            unordered.append(ffi.string(c_str))
 
-    return classes
+        # Order class to have a parent defined before their children
+        classes = []
+        while len(unordered) != len(classes):
+            for classname in unordered:
+                parentname = cls.get_class_parent(classname)
+                if not parentname or parentname in classes:
+                    if classname not in classes:
+                        classes.append(classname)
+
+        return classes
+
+    @classmethod
+    def get_class_methods(cls, classname):
+        methods = []
+        ret = ffi.new("godot_array*")
+        gd_classname = ffi.new("godot_string*")
+        lib.godot_string_new_data(gd_classname, classname.encode(), len(classname.encode()))
+        args = ffi.new("void*[2]", [gd_classname])
+        # 2nd arg should be false, which what we get by not initializing it
+        lib.godot_method_bind_ptrcall(cls._meth_get_method_list, cls._instance, args, ret)
+        for i in range(lib.godot_array_size(ret)):
+            var = lib.godot_array_get(ret, i)
+            methdict = convert_godot_dictionary(lib.godot_variant_as_dictionary(var))
+            methods.append(methdict)
+        return methods
+
+    @classmethod
+    def get_class_parent(cls, classname):
+        ret = ffi.new("godot_string*")
+        gd_classname = ffi.new("godot_string*")
+        lib.godot_string_new_data(gd_classname, classname.encode(), len(classname.encode()))
+        args = ffi.new("godot_string**", gd_classname)
+        lib.godot_method_bind_ptrcall(cls._meth_get_parent_class, cls._instance, ffi.cast("void**", args), ret)
+        c_str = lib.godot_string_c_str(ret)
+        return ffi.string(c_str)
 
 
-def get_class_parent(classname):
-    instance = lib.godot_global_get_singleton(b"ClassDB")
-    meth = lib.godot_method_bind_get_method(b"_ClassDB", b"get_parent_class")
-    ret = ffi.new("godot_string*")
-    gd_classname = ffi.new("godot_string*")
-    lib.godot_string_new_data(gd_classname, classname.encode(), len(classname.encode()))
-    args = ffi.new("godot_string**", gd_classname)
-    lib.godot_method_bind_ptrcall(meth, instance, ffi.cast("void**", args), ret)
-    c_str = lib.godot_string_c_str(ret)
-    return ffi.string(c_str)
+def convert_godot_dictionary(gddict):
+    pdict = {}
+    p_gddict = ffi.new("godot_dictionary*", gddict)
+    gdkeys = lib.godot_dictionary_keys(p_gddict)
+    p_gdkeys = ffi.new("godot_array*", gdkeys)
+    for i in range(lib.godot_array_size(p_gdkeys)):
+        p_key = lib.godot_array_get(p_gdkeys, i)
+        keystr = lib.godot_variant_as_string(p_key)
+        p_keystr = ffi.new("godot_string*", keystr)
+        c_str = lib.godot_string_c_str(p_keystr)
+        value = lib.godot_dictionary_operator_index(p_gddict, p_key)
+        # Finger crossed everything is a string...
+        valuestr = lib.godot_variant_as_string(value)
+        p_valuestr = ffi.new("godot_string*", valuestr)
+        c_valuestr = lib.godot_string_c_str(p_valuestr)
+        pdict[ffi.string(c_str)] = ffi.string(c_valuestr)
+    return pdict
 
 
-for classname in get_class_list():
+def variant_to_pyobj(gdvar):
+    pass
+
+
+for classname in ClassDB.get_class_list():
     setattr(module, classname, build_class(classname))
 
 # clist = lib.godot_get_class_list()
