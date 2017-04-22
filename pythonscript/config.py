554ac4720ebcfea26ccb7deed6eba81e2bc94489
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-04-22 10:59:08 +0200

    Remove huge output while building CPython

--- pythonscript/config.py
+++ pythonscript/config.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+import sys
 from os import path
 import subprocess
 
@@ -11,17 +13,28 @@ def can_build(platform):
     return True
 
 
+def run_and_shutup(cmd):
+    if isinstance(cmd, str):
+        cmd = cmd.split()
+    print(' '.join(cmd))
+    proc = subprocess.Popen(cmd, cwd=PY_DIR, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    if proc.returncode:
+        print(err, file=sys.stderr)
+        raise RuntimeError('Command %s failed' % cmd)
+
+
 def configure(env):
     # TODO: use ptrcall for the binding
     # env.use_ptrcall = True
     target = PY_TARGET if env["target"] != "debug" else PY_DEBUG_TARGET
     if not path.isfile(target):
-        print('Building libpython...')
+        print('Building libpython (output skipped)...')
         if not path.isfile('Makefile'):
             cmd = ['./configure', '--enable-shared', '--prefix=%s/build' % PY_DIR]
             if env["target"] == "debug":
                 cmd.append('--with-pydebug')
-            subprocess.call(cmd, cwd=PY_DIR)
-        subprocess.call(['make'], cwd=PY_DIR)
-        subprocess.call(['make', 'install'], cwd=PY_DIR)
+            run_and_shutup(cmd)
+        run_and_shutup('make')
+        run_and_shutup('make install')
         print('libpython successfully built !')
