ee58b03f42918abb2ca4e51acca47f2bb4530d16
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-07-23 00:43:35 +0200

    Add script instance function pointers

--- pythonscript/yolo/pythonscript.cpp
+++ pythonscript/yolo/pythonscript.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "pythonscript.h"
 
+// Language
 
 static void _pythonscript_init() {
 	std::cout << "init pythonscript !\n";
@@ -56,11 +57,7 @@ godot_bool _pythonscript_validate(const godot_string *p_script, int *r_line_erro
 	return true;
 }
 
-
-static const char *PYTHONSCRIPT_RECOGNIZED_EXTENSIONS[] = {"py", 0};
-static const char *PYTHONSCRIPT_RESERVED_WORDS[] = {0};
-static const char *PYTHONSCRIPT_COMMENT_DELIMITERS[] = {"#", "\"\"\"\"\"\"", 0};
-static const char *PYTHONSCRIPT_STRING_DELIMITERS[] = {"\" \"", "' '", 0};
+// Script
 
 godot_pluginscript_script_handle _pythonscript_script_init() {
 	return (godot_pluginscript_script_handle)NULL;
@@ -77,6 +74,45 @@ godot_error _pythonscript_script_reload(godot_pluginscript_script_handle p_handl
 godot_bool _pythonscript_script_can_instance(godot_pluginscript_script_handle handle) {
 	return true;
 }
+
+// Instance
+
+
+godot_pluginscript_instance_handle _pythonscript_instance_init(godot_pluginscript_script_handle script_handle, godot_object *p_owner) {
+	return NULL;
+}
+
+void _pythonscript_instance_finish(godot_pluginscript_instance_handle handle) {
+}
+
+godot_bool _pythonscript_instance_set_prop(godot_pluginscript_instance_handle handle, const godot_string *p_name, const godot_variant *p_value) {
+	return true;
+}
+godot_bool _pythonscript_instance_get_prop(godot_pluginscript_instance_handle handle, const godot_string *p_name, godot_variant *r_ret) {
+	return true;
+}
+godot_variant_type _pythonscript_instance_get_property_type(godot_pluginscript_instance_handle handle, const godot_string *p_name, godot_bool *r_is_valid) {
+	if (r_is_valid) {
+		*r_is_valid = true;
+	}
+	return GODOT_VARIANT_TYPE_NIL;
+}
+
+void _pythonscript_instance_notification(godot_pluginscript_instance_handle handle, int p_notification) {
+}
+godot_method_rpc_mode _pythonscript_instance_get_rpc_mode(godot_pluginscript_instance_handle handle, const godot_string *p_method) {
+	return 0;
+}
+godot_method_rpc_mode _pythonscript_instance_get_rset_mode(godot_pluginscript_instance_handle handle, const godot_string *p_variable) {
+	return 0;
+}
+
+// Final stuff
+
+static const char *PYTHONSCRIPT_RECOGNIZED_EXTENSIONS[] = {"py", 0};
+static const char *PYTHONSCRIPT_RESERVED_WORDS[] = {0};
+static const char *PYTHONSCRIPT_COMMENT_DELIMITERS[] = {"#", "\"\"\"\"\"\"", 0};
+static const char *PYTHONSCRIPT_STRING_DELIMITERS[] = {"\" \"", "' '", 0};
 
 godot_pluginscript_language_desc_t *godot_pluginscript_init() {
 	static godot_pluginscript_language_desc_t desc = {
@@ -97,7 +133,18 @@ godot_pluginscript_language_desc_t *godot_pluginscript_init() {
 			.init=_pythonscript_script_init,
 			.finish=_pythonscript_script_finish,
 			.reload=_pythonscript_script_reload,
-			.can_instance=_pythonscript_script_can_instance
+			.can_instance=_pythonscript_script_can_instance,
+
+			.instance_desc={
+				.init=_pythonscript_instance_init,
+				.finish=_pythonscript_instance_finish,
+				.set_prop=_pythonscript_instance_set_prop,
+				.get_prop=_pythonscript_instance_get_prop,
+				.get_property_type=_pythonscript_instance_get_property_type,
+				.notification=_pythonscript_instance_notification,
+				.get_rpc_mode=_pythonscript_instance_get_rpc_mode,
+				.get_rset_mode=_pythonscript_instance_get_rset_mode
+			}
 		}
 	};
 	return &desc;
