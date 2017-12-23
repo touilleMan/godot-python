8a008e60019916a500aad2fab5dee77ac4287f64
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-12-23 12:44:18 +0100

    Add .gdignore to release, implement dev_dyn on windows and fix export dll

--- pythonscript/pythonscript.h
+++ pythonscript/pythonscript.h
@@ -3,8 +3,8 @@
 
 #include <gdnative_api_struct.gen.h>
 
-void godot_gdnative_singleton();
-void godot_gdnative_init(godot_gdnative_init_options *options);
-void godot_gdnative_terminate();
+void GDN_EXPORT godot_gdnative_singleton();
+void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *options);
+void GDN_EXPORT godot_gdnative_terminate();
 
 #endif // PYTHONSCRIPT_H
