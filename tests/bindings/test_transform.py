10b1e2bdbc6a1c356cc19569089006457706a2b3
Gary Oberbrunner <garyo@darkstarsystems.com>
2018-03-15 13:41:22 -0400

    Removed extra arg from Transform init; with test

---
+++ tests/bindings/test_transform.py
@@ -0,0 +1,14 @@
+import pytest
+
+from godot.bindings import Transform, Basis, Vector3
+
+
+class TestTransform3D:
+
+    def test_base(self):
+        v = Transform()
+        assert type(v) == Transform
+        v2 = Transform(Basis(), Vector3(1, 2, 3))
+        assert type(v) == Transform
+        assert v2 == Transform(Basis(), Vector3(1, 2, 3))
+        assert v != v2
