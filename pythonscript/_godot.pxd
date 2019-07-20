6650c6e5952b3a883868a348a550eefdeca8c9cb
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-07-20 16:14:04 +0200

    Add support for gdnative api 1.1 and 1.2

--- pythonscript/_godot.pxd
+++ pythonscript/_godot.pxd
@@ -1,5 +1,7 @@
 from godot.gdnative_api_struct cimport (
     godot_gdnative_core_api_struct,
+    godot_gdnative_core_1_1_api_struct,
+    godot_gdnative_core_1_2_api_struct,
     godot_gdnative_ext_nativescript_api_struct,
     godot_gdnative_ext_pluginscript_api_struct,
     godot_gdnative_ext_android_api_struct,
@@ -8,6 +10,8 @@ from godot.gdnative_api_struct cimport (
 
 
 cdef const godot_gdnative_core_api_struct *gdapi
+cdef const godot_gdnative_core_1_1_api_struct *gdapi11
+cdef const godot_gdnative_core_1_2_api_struct *gdapi12
 cdef const godot_gdnative_ext_nativescript_api_struct *gdapi_ext_nativescript
 cdef const godot_gdnative_ext_pluginscript_api_struct *gdapi_ext_pluginscript
 cdef const godot_gdnative_ext_android_api_struct *gdapi_ext_android
--- pythonscript/_godot.pyx
+++ pythonscript/_godot.pyx
@@ -7,6 +7,8 @@ from godot.gdnative_api_struct cimport (
     godot_gdnative_api_struct,
     godot_gdnative_init_options,
     godot_gdnative_core_api_struct,
+    godot_gdnative_core_1_1_api_struct,
+    godot_gdnative_core_1_2_api_struct,
     godot_gdnative_ext_nativescript_api_struct,
     godot_gdnative_ext_pluginscript_api_struct,
     godot_gdnative_ext_android_api_struct,
@@ -20,6 +22,8 @@ from godot.gdnative_api_struct cimport (
 
 
 cdef const godot_gdnative_core_api_struct *gdapi
+cdef const godot_gdnative_core_1_1_api_struct *gdapi11
+cdef const godot_gdnative_core_1_2_api_struct *gdapi12
 cdef const godot_gdnative_ext_nativescript_api_struct *gdapi_ext_nativescript
 cdef const godot_gdnative_ext_pluginscript_api_struct *gdapi_ext_pluginscript
 cdef const godot_gdnative_ext_android_api_struct *gdapi_ext_android
@@ -28,12 +32,21 @@ cdef const godot_gdnative_ext_arvr_api_struct *gdapi_ext_arvr
 
 cdef api void pythonscript_register_gdapi(const godot_gdnative_init_options *options):
     global gdapi
+    global gdapi11
+    global gdapi12
     global gdapi_ext_nativescript
     global gdapi_ext_pluginscript
     global gdapi_ext_android
     global gdapi_ext_arvr
 
     gdapi = <const godot_gdnative_core_api_struct *>options.api_struct
+    gdapi11 = NULL
+    gdapi12 = NULL
+    if gdapi.next:
+        gdapi11 = <const godot_gdnative_core_1_1_api_struct *>gdapi.next
+        if gdapi11.next:
+            gdapi12 = <const godot_gdnative_core_1_2_api_struct *>gdapi11.next
+
     cdef const godot_gdnative_api_struct *ext
     for i in range(gdapi.num_extensions):
         ext = gdapi.extensions[i]
