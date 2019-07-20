5eb6c9543349556fab35c763a99fe5252a294fca
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-07-20 16:26:58 +0200

    Fully implement Vector2 bindings

--- pythonscript/godot/vector2.pxd
+++ pythonscript/godot/vector2.pxd
@@ -13,27 +13,47 @@ cdef class Vector2:
     cdef Vector2 new(godot_real x=*, godot_real y=*)
 
     cdef inline godot_vector2 *_c_vector2_ptr(Vector2 self)
-    # TODO
-    # cdef operator_equal(self, other)
-    # cdef operator_neg(self)
-    # cdef operator_add(self, val)
-    # cdef operator_substract(self, val)
-    # cdef operator_multiply_vector(self, val)
-    # cdef operator_multiply_scalar(self, val)
-    # cdef Vector2 operator_divide_vector(self, val)
-    # cdef Vector2 operator_divide_scalar(self, val)
+
+    # Operators
+    cdef inline Vector2 operator_add(self, Vector2 b)
+    cdef inline Vector2 operator_subtract(self, Vector2 b)
+    cdef inline Vector2 operator_multiply_vector(self, Vector2 b)
+    cdef inline Vector2 operator_multiply_scalar(self, godot_real b)
+    cdef inline Vector2 operator_divide_vector(self, Vector2 b)
+    cdef inline Vector2 operator_divide_scalar(self, godot_real b)
+    cdef inline bint operator_equal(self, Vector2 b)
+    cdef inline bint operator_less(self, Vector2 b)
+    cdef inline Vector2 operator_neg(self)
 
     # Properties
 
-    cdef godot_real get_x(self)
-    cdef void set_x(self, godot_real val)
-    cdef godot_real get_y(self)
-    cdef void set_y(self, godot_real val)
+    cdef inline godot_real get_x(self)
+    cdef inline void set_x(self, godot_real val)
+    cdef inline godot_real get_y(self)
+    cdef inline void set_y(self, godot_real val)
 
     # Methods
 
-    cpdef Vector2 abs(self)
+    cpdef Vector2 normalized(self)
+    cpdef godot_real length(self)
     cpdef godot_real angle(self)
+    cpdef godot_real length_squared(self)
+    cpdef bint is_normalized(self)
+    cpdef godot_real distance_to(self, Vector2 to)
+    cpdef godot_real distance_squared_to(self, Vector2 to)
     cpdef godot_real angle_to(self, Vector2 to)
     cpdef godot_real angle_to_point(self, Vector2 to)
-
+    cpdef Vector2 linear_interpolate(self, Vector2 b, godot_real t)
+    cpdef Vector2 cubic_interpolate(self, Vector2 b, Vector2 pre_a, Vector2 post_b, godot_real t)
+    cpdef Vector2 move_toward(self, Vector2 to, godot_real delta)
+    cpdef Vector2 rotated(self, godot_real phi)
+    cpdef Vector2 tangent(self)
+    cpdef Vector2 floor(self)
+    cpdef Vector2 snapped(self, Vector2 by)
+    cpdef godot_real aspect(self)
+    cpdef godot_real dot(self, Vector2 with_)
+    cpdef Vector2 slide(self, Vector2 n)
+    cpdef Vector2 bounce(self, Vector2 n)
+    cpdef Vector2 reflect(self, Vector2 n)
+    cpdef Vector2 abs(self)
+    cpdef Vector2 clamped(self, godot_real length)
--- pythonscript/godot/vector2.pyx
+++ pythonscript/godot/vector2.pyx
@@ -2,7 +2,7 @@
 
 cimport cython
 
-from _godot cimport gdapi
+from _godot cimport gdapi, gdapi12
 from .gdnative_api_struct cimport godot_vector2, godot_real
 
 
@@ -24,60 +24,91 @@ cdef class Vector2:
     def __repr__(self):
         return f"<Vector2(x={self.x}, y={self.y})>"
 
-    def __eq__(self, other):
-        cdef Vector2 _other = <Vector2?>other
-        return gdapi.godot_vector2_operator_equal(
-            self._c_vector2_ptr(), _other._c_vector2_ptr()
-        )
+    # Operators
 
-    def __ne__(self, other):
-        return not self == other
+    cdef inline Vector2 operator_add(self, Vector2 b):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_operator_add(self._c_vector2_ptr(), b._c_vector2_ptr())
+        return ret
 
-    def __neg__(self):
-        cdef ret  = Vector2.__new__()
+    cdef inline Vector2 operator_subtract(self, Vector2 b):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_operator_subtract(self._c_vector2_ptr(), b._c_vector2_ptr())
+        return ret
+
+    cdef inline Vector2 operator_multiply_vector(self, Vector2 b):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_operator_multiply_vector(self._c_vector2_ptr(), b._c_vector2_ptr())
+        return ret
+
+    cdef inline Vector2 operator_multiply_scalar(self, godot_real b):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_operator_multiply_scalar(self._c_vector2_ptr(), b)
+        return ret
+
+    cdef inline Vector2 operator_divide_vector(self, Vector2 b):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_operator_divide_vector(self._c_vector2_ptr(), b._c_vector2_ptr())
+        return ret
+
+    cdef inline Vector2 operator_divide_scalar(self, godot_real b):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_operator_divide_scalar(self._c_vector2_ptr(), b)
+        return ret
+
+    cdef inline bint operator_equal(self, Vector2 b):
+        cdef Vector2 ret  = Vector2.__new__()
+        return gdapi.godot_vector2_operator_equal(self._c_vector2_ptr(), b._c_vector2_ptr())
+
+    cdef inline bint operator_less(self, Vector2 b):
+        cdef Vector2 ret  = Vector2.__new__()
+        return gdapi.godot_vector2_operator_less(self._c_vector2_ptr(), b._c_vector2_ptr())
+
+    cdef inline Vector2 operator_neg(self):
+        cdef Vector2 ret  = Vector2.__new__()
         ret._c_vector2 = gdapi.godot_vector2_operator_neg(self._c_vector2_ptr())
         return ret
+
+    def __lt__(self, other):
+        cdef Vector2 _other = <Vector2?>other
+        return self.operator_less(_other)
+
+    def __eq__(self, other):
+        cdef Vector2 _other = <Vector2?>other
+        return self.operator_equal(_other)
+
+    def __ne__(self, other):
+        cdef Vector2 _other = <Vector2?>other
+        return not self.operator_equal(_other)
+
+    def __neg__(self):
+        return self.operator_neg()
 
     def __pos__(self):
         return self
 
     def __add__(self, val):
         cdef Vector2 _val = <Vector2?>val
-        cdef Vector2 ret  = Vector2.__new__()
-        ret._c_vector2 = gdapi.godot_vector2_operator_add(
-            (<Vector2>self)._c_vector2_ptr(), _val._c_vector2_ptr()
-        )
-        return ret
+        return self.operator_add(_val)
 
     def __sub__(self, val):
         cdef Vector2 _val = <Vector2?>val
-        cdef Vector2 ret  = Vector2.__new__()
-        ret._c_vector2 = gdapi.godot_vector2_operator_subtract(
-            (<Vector2>self)._c_vector2_ptr(), _val._c_vector2_ptr())
-        return ret
+        return self.operator_subtract(_val)
 
     def __mul__(self, val):
         cdef Vector2 _val
-        cdef Vector2 ret
 
         try:
             _val = <Vector2?>val
 
         except TypeError:
-            ret  = Vector2.__new__()
-            ret._c_vector2 = gdapi.godot_vector2_operator_multiply_scalar(
-                (<Vector2>self)._c_vector2_ptr(), val)
-            return ret
+            return self.operator_multiply_scalar(val)
 
         else:
-            ret  = Vector2.__new__()
-            ret._c_vector2 = gdapi.godot_vector2_operator_multiply_vector(
-                (<Vector2>self)._c_vector2_ptr(), _val._c_vector2_ptr())
-            return ret
+            return self.operator_multiply_vector(_val)
 
     def __truediv__(self, val):
         cdef Vector2 _val
-        cdef Vector2 ret
 
         try:
             _val = <Vector2?>val
@@ -86,75 +117,84 @@ cdef class Vector2:
             if val is 0:
                 raise ZeroDivisionError()
 
-            ret  = Vector2.__new__()
-            ret._c_vector2 = gdapi.godot_vector2_operator_divide_scalar(
-                (<Vector2>self)._c_vector2_ptr(), val)
-            return ret
+            return self.operator_divide_scalar(val)
 
         else:
             if _val.x == 0 or _val.y == 0:
                 raise ZeroDivisionError()
 
-            ret  = Vector2.__new__()
-            ret._c_vector2 = gdapi.godot_vector2_operator_divide_vector(
-                (<Vector2>self)._c_vector2_ptr(), _val._c_vector2_ptr())
-            return ret
+            return self.operator_divide_vector(_val)
 
     # Properties
 
-    cdef godot_real get_x(self):
+    cdef inline godot_real get_x(self):
         return gdapi.godot_vector2_get_x(self._c_vector2_ptr())
 
-    cdef void set_x(self, godot_real val):
+    cdef inline void set_x(self, godot_real val):
         gdapi.godot_vector2_set_x(self._c_vector2_ptr(), val)
 
-    cdef godot_real get_y(self):
+    cdef inline godot_real get_y(self):
         return gdapi.godot_vector2_get_y(self._c_vector2_ptr())
 
-    cdef void set_y(self, godot_real val):
+    cdef inline void set_y(self, godot_real val):
         gdapi.godot_vector2_set_y(self._c_vector2_ptr(), val)
 
     @property
     def x(self):
-        return gdapi.godot_vector2_get_x(self._c_vector2_ptr())
+        return self.get_x()
 
     @property
     def y(self):
-        return gdapi.godot_vector2_get_y(self._c_vector2_ptr())
+        return self.get_y()
 
     @x.setter
     def x(self, val):
-        gdapi.godot_vector2_set_x(self._c_vector2_ptr(), val)
+        self.set_x(val)
 
     @y.setter
     def y(self, val):
-        gdapi.godot_vector2_set_y(self._c_vector2_ptr(), val)
+        self.set_y(val)
 
     @property
     def width(self):
-        return self.x
+        return self.get_x()
 
     @property
     def height(self):
-        return self.y
+        return self.get_y()
 
     @width.setter
     def width(self, val):
-        self.x = val
+        self.set_x(val)
 
     @height.setter
     def height(self, val):
-        self.y = val
+        self.set_y(val)
 
     # Methods
 
-    cpdef Vector2 abs(self):
+    cpdef Vector2 normalized(self):
         cdef Vector2 ret  = Vector2.__new__()
-        ret._c_vector2 = gdapi.godot_vector2_abs(self._c_vector2_ptr())
+        ret._c_vector2 = gdapi.godot_vector2_normalized(self._c_vector2_ptr())
         return ret
+
+    cpdef godot_real length(self):
+        return gdapi.godot_vector2_length(self._c_vector2_ptr())
 
     cpdef godot_real angle(self):
         return gdapi.godot_vector2_angle(self._c_vector2_ptr())
+
+    cpdef godot_real length_squared(self):
+        return gdapi.godot_vector2_length_squared(self._c_vector2_ptr())
+
+    cpdef bint is_normalized(self):
+        return gdapi.godot_vector2_is_normalized(self._c_vector2_ptr())
+
+    cpdef godot_real distance_to(self, Vector2 to):
+        return gdapi.godot_vector2_distance_to(self._c_vector2_ptr(), to._c_vector2_ptr())
+
+    cpdef godot_real distance_squared_to(self, Vector2 to):
+        return gdapi.godot_vector2_distance_squared_to(self._c_vector2_ptr(), to._c_vector2_ptr())
 
     cpdef godot_real angle_to(self, Vector2 to):
         return gdapi.godot_vector2_angle_to(self._c_vector2_ptr(), &to._c_vector2)
@@ -162,80 +202,74 @@ cdef class Vector2:
     cpdef godot_real angle_to_point(self, Vector2 to):
         return gdapi.godot_vector2_angle_to_point(self._c_vector2_ptr(), &to._c_vector2)
 
-    # def clamped(self, length):
-    #     self._check_param_float("length", length)
-    #     gd_obj = lib.godot_vector2_clamped(self._gd_ptr, length)
-    #     return Vector2.build_from_gdobj(gd_obj)
+    cpdef Vector2 linear_interpolate(self, Vector2 b, godot_real t):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_linear_interpolate(self._c_vector2_ptr(), b._c_vector2_ptr(), t)
+        return ret
 
-    # def cubic_interpolate(self, b, pre_a, post_b, t):
-    #     self._check_param_type("b", b, Vector2)
-    #     self._check_param_type("pre_a", pre_a, Vector2)
-    #     self._check_param_type("post_b", post_b, Vector2)
-    #     self._check_param_float("t", t)
-    #     gd_obj = lib.godot_vector2_cubic_interpolate(
-    #         self._gd_ptr, b._gd_ptr, pre_a._gd_ptr, post_b._gd_ptr, t
-    #     )
-    #     return Vector2.build_from_gdobj(gd_obj)
+    cpdef Vector2 cubic_interpolate(self, Vector2 b, Vector2 pre_a, Vector2 post_b, godot_real t):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_cubic_interpolate(
+            self._c_vector2_ptr(),
+            b._c_vector2_ptr(),
+            pre_a._c_vector2_ptr(),
+            post_b._c_vector2_ptr(),
+            t
+        )
+        return ret
 
-    # def distance_squared_to(self, to):
-    #     self._check_param_type("to", to, Vector2)
-    #     return lib.godot_vector2_distance_squared_to(self._gd_ptr, to._gd_ptr)
+    cpdef Vector2 move_toward(self, Vector2 to, godot_real delta):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi12.godot_vector2_move_toward(self._c_vector2_ptr(), to._c_vector2_ptr(), delta)
+        return ret
 
-    # def distance_to(self, to):
-    #     self._check_param_type("to", to, Vector2)
-    #     return lib.godot_vector2_distance_to(self._gd_ptr, to._gd_ptr)
+    cpdef Vector2 rotated(self, godot_real phi):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_rotated(self._c_vector2_ptr(), phi)
+        return ret
 
-    # def dot(self, with_):
-    #     self._check_param_type("with_", with_, Vector2)
-    #     return lib.godot_vector2_dot(self._gd_ptr, with_._gd_ptr)
+    cpdef Vector2 tangent(self):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_tangent(self._c_vector2_ptr())
+        return ret
 
-    # def floor(self):
-    #     gd_obj = lib.godot_vector2_floor(self._gd_ptr)
-    #     return Vector2.build_from_gdobj(gd_obj)
+    cpdef Vector2 floor(self):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_floor(self._c_vector2_ptr())
+        return ret
 
-    # def floorf(self):
-    #     gd_obj = lib.godot_vector2_floorf(self._gd_ptr)
-    #     return Vector2.build_from_gdobj(gd_obj)
+    cpdef Vector2 snapped(self, Vector2 by):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_snapped(self._c_vector2_ptr(), by._c_vector2_ptr())
+        return ret
 
-    # def aspect(self):
-    #     return lib.godot_vector2_aspect(self._gd_ptr)
+    cpdef godot_real aspect(self):
+        return gdapi.godot_vector2_aspect(self._c_vector2_ptr())
 
-    # def length(self):
-    #     return lib.godot_vector2_length(self._gd_ptr)
+    cpdef godot_real dot(self, Vector2 with_):
+        return gdapi.godot_vector2_dot(self._c_vector2_ptr(), with_._c_vector2_ptr())
 
-    # def length_squared(self):
-    #     return lib.godot_vector2_length_squared(self._gd_ptr)
+    cpdef Vector2 slide(self, Vector2 n):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_slide(self._c_vector2_ptr(), n._c_vector2_ptr())
+        return ret
 
-    # def linear_interpolate(self, b, t):
-    #     self._check_param_type("b", b, Vector2)
-    #     self._check_param_float("t", t)
-    #     gd_obj = lib.godot_vector2_linear_interpolate(self._gd_ptr, b._gd_ptr, t)
-    #     return Vector2.build_from_gdobj(gd_obj)
+    cpdef Vector2 bounce(self, Vector2 n):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_bounce(self._c_vector2_ptr(), n._c_vector2_ptr())
+        return ret
 
-    # def normalized(self):
-    #     gd_obj = lib.godot_vector2_normalized(self._gd_ptr)
-    #     return Vector2.build_from_gdobj(gd_obj)
+    cpdef Vector2 reflect(self, Vector2 n):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_reflect(self._c_vector2_ptr(), n._c_vector2_ptr())
+        return ret
 
-    # def reflect(self, vec):
-    #     self._check_param_type("vec", vec, Vector2)
-    #     gd_obj = lib.godot_vector2_reflect(self._gd_ptr, vec._gd_ptr)
-    #     return Vector2.build_from_gdobj(gd_obj)
+    cpdef Vector2 abs(self):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_abs(self._c_vector2_ptr())
+        return ret
 
-    # def rotated(self, phi):
-    #     self._check_param_float("phi", phi)
-    #     gd_obj = lib.godot_vector2_rotated(self._gd_ptr, phi)
-    #     return Vector2.build_from_gdobj(gd_obj)
-
-    # def slide(self, vec):
-    #     self._check_param_type("vec", vec, Vector2)
-    #     gd_obj = lib.godot_vector2_slide(self._gd_ptr, vec._gd_ptr)
-    #     return Vector2.build_from_gdobj(gd_obj)
-
-    # def snapped(self, by):
-    #     self._check_param_type("by", by, Vector2)
-    #     gd_obj = lib.godot_vector2_snapped(self._gd_ptr, by._gd_ptr)
-    #     return Vector2.build_from_gdobj(gd_obj)
-
-    # def tangent(self):
-    #     gd_obj = lib.godot_vector2_tangent(self._gd_ptr)
-    #     return Vector2.build_from_gdobj(gd_obj)
+    cpdef Vector2 clamped(self, godot_real length):
+        cdef Vector2 ret  = Vector2.__new__()
+        ret._c_vector2 = gdapi.godot_vector2_clamped(self._c_vector2_ptr(), length)
+        return ret
