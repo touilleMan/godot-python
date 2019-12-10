c32bf38f945377f54f21f62207aab9dfbf154840
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-12-10 23:46:52 +0100

    Correct tests/bindings/test_starimport.py

--- tests/bindings/test_starimport.py
+++ tests/bindings/test_starimport.py
@@ -1,8 +1,11 @@
 # This test is in it own file to protect other tests from the `import *` side effects
 from godot.bindings import *
 
+# Class with trailing underscore are provided on star import
+from godot.bindings import _OS, _ProjectSettings
+
 
 def test_starimport():
     assert issubclass(Node, Object)
-    assert isinstance(PhysicsServer, _PhysicsServer)
-    assert isinstance(Engine, _Engine)
+    assert isinstance(OS, _OS)
+    assert isinstance(ProjectSettings, _ProjectSettings)
