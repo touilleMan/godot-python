4b40eb8e2128f3bc73f030aa6c49051a1619c648
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-12-15 11:10:55 +0100

    Remove print in tests/bindings/main.py

--- tests/bindings/main.py
+++ tests/bindings/main.py
@@ -12,11 +12,9 @@ def set_current_node(node):
     global __current_node
     assert __current_node is None
     __current_node = node
-    print("SET NODE:", __current_node, id(set_current_node))
 
 
 def get_current_node():
-    print("GET NODE:", __current_node, id(set_current_node))
     return __current_node
 
 
