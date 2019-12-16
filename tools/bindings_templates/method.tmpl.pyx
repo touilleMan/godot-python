8fa565f3a4ba204b4361696a9cfe1ba5f9a2ad98
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-12-16 16:13:45 +0100

    Add support for str type on GDString params in bindings

--- tools/bindings_templates/method.tmpl.pyx
+++ tools/bindings_templates/method.tmpl.pyx
@@ -20,9 +20,13 @@ cdef godot_method_bind *{{ get_method_bind_register_name(cls, method) }} = gdapi
 {% macro render_method_signature(method) %}
 {{ method["name"] }}(self,
 {%- for arg in method["arguments"] %}
+{%- if arg["type"] == "godot_string" %}
+ object {{ arg["name"] }}
+{%- else %}
  {{ arg["type_specs"]["binding_type"] }} {{ arg["name"] }}
 {%- if not arg["type_specs"]["is_base_type"] %}
   not None
+{%- endif %}
 {%- endif %}
 ,
 {%- endfor %}
@@ -56,7 +60,18 @@ cdef const void *{{ argsval }}[{{ method["arguments"] | length }}]
 {% for arg in method["arguments"] %}
 {% set i = loop.index - 1 %}
 # {{ arg["type"] }} {{ arg["name"] }}
-{% if arg["type_specs"]["is_object"] %}
+{% if arg["type"] == "godot_string" %}
+cdef GDString __gdstr_{{ arg["name"] }}
+try:
+    __gdstr_{{ arg["name"] }} = <GDString?>{{ arg["name"] }}
+except TypeError:
+    if isinstance({{ arg["name"] }}, str):
+        __gdstr_{{ arg["name"] }} = GDString.__new__(GDString)
+        pyobj_to_godot_string({{ arg["name"] }}, &__gdstr_{{ arg["name"] }}._gd_data)
+    else:
+        raise TypeError("{{ arg['name'] }}")
+{{ argsval }}[{{ i }}] = <void*>(&__gdstr_{{ arg["name"] }}._gd_data)
+{% elif arg["type_specs"]["is_object"] %}
 {{ argsval }}[{{ i }}] = <void*>(&{{ arg["name"] }}._gd_ptr)
 {% elif arg["type"] == "godot_variant" %}
 cdef godot_variant __var_{{ arg["name"] }}
