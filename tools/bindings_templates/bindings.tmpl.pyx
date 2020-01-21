56d6b477bb7f5fd8c3adcd750515eca72b813b2b
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-01-21 11:01:16 +0800

    Improve style in bindings_templates's class stuff

--- tools/bindings_templates/bindings.tmpl.pyx
+++ tools/bindings_templates/bindings.tmpl.pyx
@@ -4,34 +4,14 @@
 from godot._hazmat.gdnative_api_struct cimport *
 from godot._hazmat.gdapi cimport pythonscript_gdapi10 as gdapi10
 from godot._hazmat.conversion cimport *
-from godot.builtins cimport (
-	AABB,
-	Array,
-	Basis,
-	Color,
-	Dictionary,
-	GDString,
-	NodePath,
-	Plane,
-	Quat,
-	Rect2,
-	RID,
-	Transform,
-	Transform2D,
-	Vector2,
-	Vector3,
-    PoolIntArray,
-    PoolRealArray,
-    PoolByteArray,
-    PoolVector2Array,
-    PoolVector3Array,
-    PoolColorArray,
-    PoolStringArray,
-)
+from godot.builtins cimport *
 
 ### Classes ###
 
-{% include "classes.tmpl.pyx" %}
+{% from 'class.tmpl.pyx' import render_class -%}
+{%- for cls in classes %}
+{{ render_class(cls) }}
+{%- endfor %}
 
 ### Singletons ###
 
--- tools/bindings_templates/classes.tmpl.pyx
+++
@@ -1,4 +0,0 @@
-{% from 'class.tmpl.pyx' import render_class %}
-{% for cls in classes %}
-{{ render_class(cls) }}
-{% endfor %}
