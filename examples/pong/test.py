d16474cdd3ac605ed4cd8be236b45a1cf19b15f7
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-01-25 15:48:29 +0800

    Fix space indendation in examples/pong/test.py

--- examples/pong/test.py
+++ examples/pong/test.py
@@ -1,20 +1,20 @@
 class K:
-	@property
-	def p(self):
-		print('getter called')
-		return self._p
+    @property
+    def p(self):
+        print('getter called')
+        return self._p
 
-	@p.setter
-	def p(self, value):
-		print('setter called')
-		self._p = value
+    @p.setter
+    def p(self, value):
+        print('setter called')
+        self._p = value
 
-	def __setattr__(self, name, value):
-		print('setattr called')
-		object.__setattr__(self, name, value)
+    def __setattr__(self, name, value):
+        print('setattr called')
+        object.__setattr__(self, name, value)
 
-	def __init__(self):
-		self._p = 42
+    def __init__(self):
+        self._p = 42
 
 k = K()
 print(k.p)
