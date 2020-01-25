932540cbb5d1e3f55d5bb3c1100555ef4143ed8c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-01-25 15:59:03 +0800

    Improve test_transform.py

--- tests/bindings/test_transform.py
+++ tests/bindings/test_transform.py
@@ -3,11 +3,15 @@ import pytest
 from godot import Transform, Basis, Vector3
 
 
-class TestTransform3D:
-    def test_base(self):
-        v = Transform()
-        assert type(v) == Transform
-        v2 = Transform.from_basis_origin(Basis(), Vector3(1, 2, 3))
-        assert type(v) == Transform
-        assert v2 == Transform.from_basis_origin(Basis(), Vector3(1, 2, 3))
-        assert v != v2
+def test_base():
+    v = Transform()
+    assert type(v) == Transform
+    v2 = Transform.from_basis_origin(Basis(), Vector3(1, 2, 3))
+    assert type(v) == Transform
+    assert v2 == Transform.from_basis_origin(Basis(), Vector3(1, 2, 3))
+    assert v != v2
+
+
+def test_repr():
+    v = Transform()
+    assert repr(v).startswith("<Transform(")
