44dcba420cc50d714caad90d17152ba3f2add638
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-04-26 11:14:17 +0200

    Protect segfault in bindings.pyx when godot singleton cannot be loaded

--- tools/bindings_templates/singletons.tmpl.pyx
+++ tools/bindings_templates/singletons.tmpl.pyx
@@ -6,8 +6,11 @@
 {% endfor %}
 {% endmacro %}
 
+cdef void *ptr
 {% call(cls) iter_singletons(classes) %}
-{{ cls["singleton_name"] }} = {{ cls["name"] }}.from_ptr(
-	gdapi10.godot_global_get_singleton("{{ cls['singleton_name'] }}")
-)
+ptr = gdapi10.godot_global_get_singleton("{{ cls['singleton_name'] }}")
+if ptr:
+    {{ cls["singleton_name"] }} = {{ cls["name"] }}.from_ptr(
+        gdapi10.godot_global_get_singleton("{{ cls['singleton_name'] }}")
+    )
 {% endcall %}
