770057522e6c2c876d128862c8722c65cefbdc46
Matheus Salvia <matheus2740@gmail.com>
2020-05-28 15:35:10 -0300

    fix style

--- addons/repl/InputBox.py
+++ addons/repl/InputBox.py
@@ -1,32 +1,31 @@
 from godot import exposed, export
 from godot import *
 
-
 @exposed(tool=True)
 class InputBox(LineEdit):
 
-	# segfaults
-	#def _input(self, event):
-		# if event is InputEventKey and event.pressed:
-		# 	if event.scancode == KEY_UP:
-		# 		print("UP was pressed")
-	
-	#also segfaults
-	# def _gui_input(self, event):
-	# 	pass
+    # segfaults
+    # def _input(self, event):
+    # if event is InputEventKey and event.pressed:
+    # 	if event.scancode == KEY_UP:
+    # 		print("UP was pressed")
 
-	def _enter_tree(self):
-		self.had_focus = False
+    # also segfaults
+    # def _gui_input(self, event):
+    # 	pass
 
-	def _process(self, _delta):
-		# Hacky, but _input is segfaulting right now
-		if Input.is_action_just_pressed('ui_up') and self.had_focus:
-			self.get_parent().get_parent().up_pressed()
-		
-		if Input.is_action_just_pressed('ui_down') and self.had_focus:
-			self.get_parent().get_parent().down_pressed()
+    def _enter_tree(self):
+        self.had_focus = False
 
-		self.had_focus = self.has_focus()
+    def _process(self, _delta):
+        # Hacky, but _input is segfaulting right now
+        if Input.is_action_just_pressed('ui_up') and self.had_focus:
+            self.get_parent().get_parent().up_pressed()
 
-	def _ready(self):
-		pass
+        if Input.is_action_just_pressed('ui_down') and self.had_focus:
+            self.get_parent().get_parent().down_pressed()
+
+        self.had_focus = self.has_focus()
+
+    def _ready(self):
+        pass
--- addons/repl/PythonREPL.py
+++ addons/repl/PythonREPL.py
@@ -3,85 +3,86 @@ import code
 from godot import exposed, export
 from godot import *
 
+
 @exposed(tool=True)
 class PythonREPL(VBoxContainer):
 
-	def _enter_tree(self):
-		self.history = []
-		self.selected_history = 0
-		self.output_box = self.get_node("OutputBox")
-		font = ResourceLoader.load("res://addons/pythonscript_repl/Hack_Regular.tres")
-		self.output_box.add_font_override("normal_font", font)
-		self.output_box.add_font_override("mono_font", font)
-		self.run_button = self.get_node("FooterContainer/RunButton")
-		self.copy_button = self.get_node("HeaderContainer/CopyButton")
-		self.copy_button.connect("pressed", self, "copy")
-		self.clear_button = self.get_node("HeaderContainer/ClearButton")
-		self.clear_button.connect("pressed", self, "clear")
-		self.input_box = self.get_node("FooterContainer/InputBox")
-		self.input_box.connect("text_entered", self, "execute")
-		self.run_button.connect("pressed", self, "execute")
-		self.interpreter_context = {"__name__": "__console__", "__doc__": None}
-		self.interpreter = code.InteractiveConsole(self.interpreter_context)
-		self.more = False
-		if getattr(sys.stdout, "add_callback", None) is not None:
-			sys.stdout.add_callback(self.output_line)
-			# sys.stderr.add_callback(self.output_line)
-		else:
-			self.output_line("It seems IO Streams Capture is disabled.")
-			self.output_line("In order to see the output of commands, go to:")
-			self.output_line("Project > Project Settings > Python Script > Io Streams Capture")
-			self.output_line("and enable Io Streams Capture.")
-	
-	def _exit_tree(self):
-		if getattr(sys.stdout, "remove_callback", None) is not None:
-			sys.stdout.remove_callback(self.output_line)
-			# sys.stderr.remove_callback(self.output_line)
-		
-	def _ready(self):
-		pass
-	
-	def output_line(self, line):
-		self.output_box.push_mono()
-		self.output_box.add_text(line)
-		self.output_box.newline()
-		self.output_box.pop()
-	
-	def remove_last_line(self):
-		self.output_box.remove_line(self.output_box.get_line_count() - 2)
-		self.output_box.scroll_to_line(self.output_box.get_line_count() - 1)
-		
-	def execute(self, *args, **kwargs):
-		string = self.input_box.get_text()
-		# avoid adding multiple repeated entries to the command history
-		if not (len(self.history) > 0 and self.history[-1] == string):
-			self.history.append(string)
-		self.selected_history = 0
-		self.input_box.clear()
-		linestart = "... " if self.more else ">>> "
-		self.output_line(linestart + str(string))
-		self.more = self.interpreter.push(str(string))
+    def _enter_tree(self):
+        self.history = []
+        self.selected_history = 0
+        self.output_box = self.get_node("OutputBox")
+        font = ResourceLoader.load("res://addons/pythonscript_repl/Hack_Regular.tres")
+        self.output_box.add_font_override("normal_font", font)
+        self.output_box.add_font_override("mono_font", font)
+        self.run_button = self.get_node("FooterContainer/RunButton")
+        self.copy_button = self.get_node("HeaderContainer/CopyButton")
+        self.copy_button.connect("pressed", self, "copy")
+        self.clear_button = self.get_node("HeaderContainer/ClearButton")
+        self.clear_button.connect("pressed", self, "clear")
+        self.input_box = self.get_node("FooterContainer/InputBox")
+        self.input_box.connect("text_entered", self, "execute")
+        self.run_button.connect("pressed", self, "execute")
+        self.interpreter_context = {"__name__": "__console__", "__doc__": None}
+        self.interpreter = code.InteractiveConsole(self.interpreter_context)
+        self.more = False
+        if getattr(sys.stdout, "add_callback", None) is not None:
+            sys.stdout.add_callback(self.output_line)
+        # sys.stderr.add_callback(self.output_line)
+        else:
+            self.output_line("It seems IO Streams Capture is disabled.")
+            self.output_line("In order to see the output of commands, go to:")
+            self.output_line("Project > Project Settings > Python Script > Io Streams Capture")
+            self.output_line("and enable Io Streams Capture.")
 
-	def up_pressed(self):
-		if len(self.history) >= abs(self.selected_history - 1):
-			self.selected_history -= 1
-			self.input_box.clear()
-			self.input_box.set_text(self.history[self.selected_history])
-		self.input_box.grab_focus()
-	
-	def down_pressed(self):
-		if self.selected_history + 1 == 0:
-			self.selected_history += 1
-			self.input_box.clear()
-		elif self.selected_history + 1 < 0:
-			self.selected_history += 1
-			self.input_box.clear()
-			self.input_box.set_text(self.history[self.selected_history])
+    def _exit_tree(self):
+        if getattr(sys.stdout, "remove_callback", None) is not None:
+            sys.stdout.remove_callback(self.output_line)
+        # sys.stderr.remove_callback(self.output_line)
 
-		self.input_box.grab_focus()
-	
-	def copy(self):
-		pass
+    def _ready(self):
+        pass
 
-	def clear(self):
-		self.output_box.clear()
+    def output_line(self, line):
+        self.output_box.push_mono()
+        self.output_box.add_text(line)
+        self.output_box.newline()
+        self.output_box.pop()
+
+    def remove_last_line(self):
+        self.output_box.remove_line(self.output_box.get_line_count() - 2)
+        self.output_box.scroll_to_line(self.output_box.get_line_count() - 1)
+
+    def execute(self, *args, **kwargs):
+        string = self.input_box.get_text()
+        # avoid adding multiple repeated entries to the command history
+        if not (len(self.history) > 0 and self.history[-1] == string):
+            self.history.append(string)
+        self.selected_history = 0
+        self.input_box.clear()
+        linestart = "... " if self.more else ">>> "
+        self.output_line(linestart + str(string))
+        self.more = self.interpreter.push(str(string))
+
+    def up_pressed(self):
+        if len(self.history) >= abs(self.selected_history - 1):
+            self.selected_history -= 1
+            self.input_box.clear()
+            self.input_box.set_text(self.history[self.selected_history])
+        self.input_box.grab_focus()
+
+    def down_pressed(self):
+        if self.selected_history + 1 == 0:
+            self.selected_history += 1
+            self.input_box.clear()
+        elif self.selected_history + 1 < 0:
+            self.selected_history += 1
+            self.input_box.clear()
+            self.input_box.set_text(self.history[self.selected_history])
+
+        self.input_box.grab_focus()
+
+    def copy(self):
+        pass
+
+    def clear(self):
+        self.output_box.clear()
