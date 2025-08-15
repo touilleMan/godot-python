038b7b180f9ed085d66c16da86c18859d5cf8851
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-08-15 19:05:42 +0200

    Switch to uv

--- make.py
+++ make.py
@@ -109,12 +109,12 @@ class Cmd(Op):
 
 COMMANDS: dict[tuple[str, ...], Union[Op, tuple[Op, ...]]] = {
     ("init", "i"): (
-        Cmd(f"meson setup {BUILD_DIR}"),
-        Cmd(f"meson compile -C {BUILD_DIR}"),
+        Cmd(f"uv run meson setup {BUILD_DIR}"),
+        Cmd(f"uv run meson compile -C {BUILD_DIR}"),
     ),
-    ("rebuild", "r"): (Cmd(f"meson compile -C {BUILD_DIR}"),),
+    ("rebuild", "r"): (Cmd(f"uv run meson compile -C {BUILD_DIR}"),),
     ("tests", "t"): (
-        Cmd(f"python tests/run.py --build-dir={BUILD_DIR} {{extra_cmd_args}} -- --headless"),
+        Cmd(f"uv run python tests/run.py --build-dir={BUILD_DIR} {{extra_cmd_args}} -- --headless"),
     ),
 }
 
