a2cc730e9d6ef6e81c958d649ff261a073b1c303
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-08-17 10:19:46 +0200

    Add global enum and constants tests in `tests/4-use-godot-from-python/tests/test_builtins.py`

--- tests/4-use-godot-from-python/tests/test_builtins.py
+++ tests/4-use-godot-from-python/tests/test_builtins.py
@@ -1,7 +1,18 @@
+import clodotest
 import godot
 
 
-def test_vector2():
-    v = godot.Vector2(x=1, y=2)
-    assert v.x == 1
-    assert v.y == 2
+@clodotest.xfail(reason="TODO: Godot enum from Python is still WIP")
+def test_global_enum():
+    assert godot.Error.OK == 0
+    assert godot.Error.FAILED == 1
+
+    # Since `Variant` is not defined in `extension_api.json`, it's enums
+    # are defined among the global ones...
+    assert godot.Variant.Type.NIL == 0
+    assert godot.Variant.Operator.OP_MAX == 25
+
+
+def test_global_constant():
+    # Nothing to do: `extension_api.json`'s `global_constants` entry is empty so far...
+    pass
