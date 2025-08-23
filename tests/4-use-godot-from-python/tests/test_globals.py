508d0485040c8a04e08edb3f01675bd3ecd3940a
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-08-23 15:33:57 +0200

    Add support for global enums in builtins

--- tests/4-use-godot-from-python/tests/test_globals.py
+++ tests/4-use-godot-from-python/tests/test_globals.py
@@ -1,16 +1,18 @@
-import clodotest
+from clodotest import assert_eq
 import godot
 
 
-@clodotest.xfail(reason="TODO: Godot enum from Python is still WIP")
 def test_global_enum():
-    assert godot.Error.OK == 0
-    assert godot.Error.FAILED == 1
+    assert_eq(godot.Error.OK, 0)
+    assert_eq(godot.Error.FAILED, 1)
+    assert_eq(godot.Key.KEY_Z, 90)
+    # We expect the enum values not to leak outside of their enum type
+    assert not hasattr(godot, "OK")
 
     # Since `Variant` is not defined in `extension_api.json`, it's enums
     # are defined among the global ones...
-    assert godot.Variant.Type.NIL == 0
-    assert godot.Variant.Operator.OP_MAX == 25
+    assert godot.VariantType.TYPE_NIL == 0
+    assert godot.VariantOperator.OP_ADD == 6
 
 
 def test_global_constant():
