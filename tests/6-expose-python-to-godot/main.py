768399b15a10b3243ab1b64024373efd5238d139
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-09-23 10:06:10 +0200

    Correct `tests/6-expose-python-to-godot/expected.output`

--- tests/6-expose-python-to-godot/main.py
+++ tests/6-expose-python-to-godot/main.py
@@ -5,4 +5,6 @@ from godot.classes import Node
 @exposed
 class Main(Node):
     def _ready(self):
+        print("Python: _ready entered", flush=True)
         self.get_tree().quit()
+        print("Python: _ready done", flush=True)
